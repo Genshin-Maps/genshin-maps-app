0e93be5d3d94224b40035ea7c9b506162c29672b
juhyeon-cha <28646684+juhyeon-cha@users.noreply.github.com>
2023-05-07 11:42:42 +0900

    electron 실행 시 오류/주의 메시지 원인 수정

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,21 +1,42 @@
 import { contextBridge, ipcRenderer } from "electron";
 import { electronAPI } from "@electron-toolkit/preload";
+import type { IpcRenderer, IpcRendererEvent } from "electron";
 import type { Bridge } from "@t/renderer";
+import type { CvatTrackData } from "@t/backend";
+
+class IpcHelper {
+    ipcRenderer: IpcRenderer;
+
+    constructor(ipcRenderer: IpcRenderer) {
+        this.ipcRenderer = ipcRenderer;
+    }
+
+    invoke(channel: string) {
+        return (...args: any[]) => this.ipcRenderer.invoke(channel, ...args).catch((err) => console.error(err));
+    }
+
+    on(channel: string) {
+        return (callback: (event: IpcRendererEvent, args: CvatTrackData) => void) => ipcRenderer.on(channel, callback);
+    }
+}
+
+const ipcHelper = new IpcHelper(ipcRenderer);
 
 // 프런트엔드에서 사용할 수 있는 API를 노출시킨다.
 // preload.js에서는 context가 renderer process이므로, backend에서 사용할 수 있는 전역 값 등을 사용할 수 없다.
 // win.{apiKey}.{apiFunction}((event, value) => {}) 형태로 사용할 수 있다.
 const bridge: Bridge = {
-    getAppInfo: () => ipcRenderer.invoke("get-app-info"),
-    getConfig: () => ipcRenderer.invoke("get-config"),
-    setConfig: (config) => ipcRenderer.invoke("set-config", config),
+    getAppInfo: ipcHelper.invoke("get-app-info"),
+    getConfig: ipcHelper.invoke("get-config"),
+    setConfig: ipcHelper.invoke("set-config"),
 
-    onUpdate: (callback) => ipcRenderer.on("on-update", callback),
+    onUpdate: ipcHelper.on("on-update"),
 
-    startTrack: () => ipcRenderer.invoke("start-track"),
-    stopTrack: () => ipcRenderer.invoke("stop-track"),
-    onTrack: (callback) => ipcRenderer.on("track", callback),
+    startTrack: ipcHelper.invoke("start-track"),
+    stopTrack: ipcHelper.invoke("stop-track"),
+    onTrack: ipcHelper.on("track"),
 };
+
 if (process.contextIsolated) {
     try {
         contextBridge.exposeInMainWorld("electron", electronAPI);
